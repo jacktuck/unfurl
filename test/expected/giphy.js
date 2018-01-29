92d85c5148ec3bf7d52de0c4d9bdc64d8cd69a09
Jack Tuck <jack-tuck@hotmail.com>
2018-01-29 04:35:33 +0000

    fix tests

--- test/expected/giphy.js
+++
@@ -1,1 +0,0 @@
-module.exports = { other: { _type: 'image/gif' } }
---
+++ test/input-output.test.js
@@ -0,0 +1,74 @@
+const serve = require('serve')
+const delay = require('delay');
+
+const unfurl = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-as-promised'))
+
+const fs = require('fs')
+const tests = [
+  'bbc',
+  'soundcloud',
+  'youtube',
+  'medium',
+  'twitter',
+  'imgur',
+  'giphy'
+]
+
+const srcs = Object.assign({}, ...tests.map(test => ({
+  [test]: {
+    url: `http://localhost:3000/${test}.source.html`,
+    expected: JSON.parse(fs.readFileSync(`${__dirname}/${test}.expected.json`).toString())
+  }
+})))
+
+describe('input-output', function () {
+  let server 
+
+  before(function (done) {
+    server = serve(__dirname, {
+      port: 3000,
+      ignore: ['node_modules']
+    })
+
+    delay(1000).then(_ => done())
+    
+  })
+
+  after(function () {
+    server.stop()
+  })
+
+  it('should resolve metadata for twitter', function () {
+    return expect(unfurl(srcs.twitter.url)).to.be.fulfilled.and.to.eventually.become(srcs.twitter.expected)
+  })
+
+  it('should resolve metadata for bbc', function () {
+    return expect(unfurl(srcs.bbc.url)).to.be.fulfilled.and.to.eventually.become(srcs.bbc.expected)
+  })
+
+  it('should resolve metadata for medium', function () {
+    return expect(unfurl(srcs.medium.url)).to.be.fulfilled.and.to.eventually.become(srcs.medium.expected)
+  })
+
+  it('should resolve metadata for youtube', function () {
+    return expect(unfurl(srcs.youtube.url)).to.be.fulfilled.and.to.eventually.become(srcs.youtube.expected)
+  })
+
+  it('should resolve metadata for imgur', function () {
+    return expect(unfurl(srcs.imgur.url)).to.be.fulfilled.and.to.eventually.become(srcs.imgur.expected)
+  })
+
+  it('should resolve metadata for giphy', function () {
+    return expect(unfurl(srcs.giphy.url)).to.be.fulfilled.and.to.eventually.become(srcs.giphy.expected)
+  })
+
+  after(function() {
+    // app.close()
+    // done()
+    // lol
+    // process.exit()
+  })
+})
