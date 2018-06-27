7fb02015be87c000024edebd0a8057fd6817ac52
Jack Tuck <jack-tuck@hotmail.com>
2018-06-27 03:28:08 +0100

    linting

--- test/generate.js
+++ test/generate.js
@@ -10,33 +10,32 @@ const links = require('./links')
 
 const serve = serveStatic('./')
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
   const handler = finalhandler(req, res)
   serve(req, res, handler)
 })
 
-server.listen(1337, async function (err) {
+server.listen(1337, async function () {
   try {
     console.log('Listening on port 1337')
 
     for (const [name, link] of links) {
       console.log('link', link)
-  
+
       const html = await fetch(link).then(res => res.text())
-  
+
       fs.writeFileSync(`${name}.source.html`, html)
-        
+
       console.log('writing json to', `${name}.expected.json`)
       const json = await unfurl(`http://localhost:1337/${name}.source.html`)
         .then(res => JSON.stringify(res, null, 4))
 
       fs.writeFileSync(`${name}.expected.json`, json)
     }
-  
+
     server.close()
   } catch (err) {
     console.log('ERR', err)
     server.close()
   }
 })
-
