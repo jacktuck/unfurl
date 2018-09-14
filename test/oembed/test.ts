af4eef40012a65af574927579748c8bc11c63666
jt <jack.tuck@crugo.com>
2018-09-14 16:26:04 +0100

    wip

--- test/oembed/test.ts
+++ test/oembed/test.ts
@@ -1,1 +1,38 @@
-test('', () => {})
+import unfurl from '../../src/'
+import fs from 'fs'
+import http from 'http'
+import iconv from 'iconv-lite'
+import jschardet from 'jschardet'
+import TestServer from '../server'
+
+const port = 9000
+const baseUrl = `http://localhost:${port}`
+
+beforeAll(then => TestServer.listen(port, then))
+afterAll(then => TestServer.close(then))
+
+test('should get oembed data', async () => {
+  // console.log('starting test')
+  const result = await unfurl(baseUrl + '/html/oembed')
+
+  // console.log('RESULT!!!', JSON.stringify(result, null, 2))
+  const expected = {
+    version: '1.0',
+    provider_url: 'https://gfycat.com',
+    provider_name: 'Gfycat',
+    type: 'video',
+    title: 'The creation of a marble sculpture',
+    html: `<div style='position:relative;padding-bottom:100.0%'><iframe src='https://gfycat.com/ifr/ImpressionableWaterloggedAbalone' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div>`,
+    height: 640,
+    width: 640,
+    thumbnail: [
+      {
+        url: 'https://gfycat.com/uk/gifs/detail/impressionablewaterloggedabalone',
+        width: 200,
+        height: 200
+      }
+    ]
+  }
+
+  expect(result.oEmbed).toEqual(expected)
+})
--- test/server.ts
+++ test/server.ts
@@ -5,6 +5,36 @@ export default http.createServer(function (req, res) {
   try {
     const route = req.url
 
+    // console.log('GOT ROUTE', route)
+
+    if (route === '/json/oembed.json') {
+      // console.log('INSIDE ROUTE /json/oembed')
+      const json = fs.readFileSync(__dirname + '/oembed/oembed.json')
+
+      // console.log('FOOO!')
+      // console.log('json', json.toString())
+
+      res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(json),
+        'Content-Type': `application/json`
+      })
+      res.write(json)
+      res.end()
+    }
+
+    if (route === '/html/oembed') {
+      // console.log('INSIDE ROUTE /html/oembed')
+      const html = fs.readFileSync(__dirname + '/oembed/oembed.html')
+      // console.log('html', html.toString())
+
+      res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(html),
+        'Content-Type': `text/html`
+      })
+      res.write(html)
+      res.end()
+    }
+
     if (route === '/multi') {
       const html = fs.readFileSync(__dirname + '/twitter_card/multi.html')
 
@@ -71,7 +101,7 @@ export default http.createServer(function (req, res) {
       res.end()
     }
   } catch (err) {
-    console.log('ERR', err)
+    // console.log('ERR', err)
     res.writeHead(500, err.message)
     res.end()
   }
