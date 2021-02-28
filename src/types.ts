915bf8ac60dca6940bb2270ef4c26bb4431bd96e
Henrikas Kuzmickas <henrikask@wix.com>
2021-02-28 12:54:55 +0200

    fix: typings (#74)

--- src/types.ts
+++ src/types.ts
@@ -14,10 +14,10 @@ export type Opts = {
 }
 
 export type Metadata = {
-  title: string
-  description: string
-  keywords: string[]
-  favicon: string
+  title?: string
+  description?: string
+  keywords?: string[]
+  favicon?: string
   oEmbed?: {
     type: 'photo' | 'video' | 'link' | 'rich'
     version?: string
@@ -67,7 +67,7 @@ export type Metadata = {
       url: string
       alt: string
     }[]
-  }[]
+  }
   open_graph: {
     title: string
     type: string
@@ -95,5 +95,5 @@ export type Metadata = {
       width?: number
       tags?: string[]
     }[]
-  }[]
+  }
 }
