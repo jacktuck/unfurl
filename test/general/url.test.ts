f86be63c352717222f845a243497e30b337e6c81
jack <8209433+jacktuck@users.noreply.github.com>
2021-09-08 20:13:11 +0100

    fix test case

--- test/general/url.test.ts
+++ test/general/url.test.ts
@@ -4,19 +4,13 @@ import { unfurl } from '../../src/'
 test('should not throw when provided non-ascii url', async () => {
     expect.assertions(1)
 
-    nock('http://localhost')
-        .get(/.*/)
-        .reply(200, '', { 
-            'Content-Type': 'text/html' 
-        })
-
     let err
     try {
         await unfurl('http://localhost/日本語urlってどうよ')
     } catch (e) {
         err = e
     } finally {
-        expect(err).not.toBeDefined()
+        expect(err).not.toMatchObject({ code: 'ERR_UNESCAPED_CHARACTERS' })
     }
 })
 
